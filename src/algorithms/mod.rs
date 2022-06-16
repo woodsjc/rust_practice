53c2bcac669149244829462cfe2ca81189dc54ce
woodsjc <woodsjc@gmail.com>
2022-06-15 20:29:48 -0500

    reorganize code into algorithm directory

---
+++ src/algorithms/mod.rs
@@ -0,0 +1,11 @@
+pub mod add_two_numbers;
+pub mod delete_operation_for_two_strings;
+pub mod longest_palindrome;
+pub mod longest_substring_without_repeating_characters;
+pub mod maximum_erasure_value;
+pub mod maximum_subarray_min_product;
+pub mod minimum_operations_to_reduce_x_to_zero;
+pub mod sum_of_subarray_minimums;
+pub mod sum_of_total_strength_of_wizards;
+pub mod triangle;
+pub mod two_sums;
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,34 @@
-mod add_two_numbers;
-mod delete_operation_for_two_strings;
-mod longest_palindrome;
-mod longest_substring_without_repeating_characters;
-mod maximum_erasure_value;
-mod maximum_subarray_min_product;
-mod minimum_operations_to_reduce_x_to_zero;
-mod sum_of_subarray_minimums;
-mod sum_of_total_strength_of_wizards;
-mod triangle;
-mod two_sums;
+mod algorithms;
 
 fn main() {
-    let s = two_sums::two_sums(vec![1, 2, 3, 4, 5], 7);
+    let s = algorithms::two_sums::two_sums(vec![1, 2, 3, 4, 5], 7);
     print!("{:?}\n", s);
-    let s = two_sums::two_sums(vec![3, 3], 6);
+    let s = algorithms::two_sums::two_sums(vec![3, 3], 6);
     print!("{:?}\n", s);
 
-    let s = longest_palindrome::longest_palindrome("sssswwwwweeee");
+    let s = algorithms::longest_palindrome::longest_palindrome("sssswwwwweeee");
     print!("Longest palindrome: {}\n", s);
 
-    let n1 = Some(Box::new(add_two_numbers::ListNode {
+    let n1 = Some(Box::new(algorithms::add_two_numbers::ListNode {
         val: 2,
-        next: Some(Box::new(add_two_numbers::ListNode {
+        next: Some(Box::new(algorithms::add_two_numbers::ListNode {
             val: 4,
-            next: Some(Box::new(add_two_numbers::ListNode { val: 3, next: None })),
+            next: Some(Box::new(algorithms::add_two_numbers::ListNode {
+                val: 3,
+                next: None,
+            })),
         })),
     }));
-    let n2 = Some(Box::new(add_two_numbers::ListNode {
+    let n2 = Some(Box::new(algorithms::add_two_numbers::ListNode {
         val: 5,
-        next: Some(Box::new(add_two_numbers::ListNode {
+        next: Some(Box::new(algorithms::add_two_numbers::ListNode {
             val: 6,
-            next: Some(Box::new(add_two_numbers::ListNode { val: 4, next: None })),
+            next: Some(Box::new(algorithms::add_two_numbers::ListNode {
+                val: 4,
+                next: None,
+            })),
         })),
     }));
-    let s = add_two_numbers::add_two_numbers(n1, n2);
+    let s = algorithms::add_two_numbers::add_two_numbers(n1, n2);
     print!("Add two linked lists: {:?}\n", s);
 }
